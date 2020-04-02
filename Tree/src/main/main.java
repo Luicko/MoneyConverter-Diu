59b9a2d872bf52b68e7fd0ba2da09bf794e6cd1f
Prashant-JT <prashantjeswani8@gmail.com>
2020-04-02 04:14:05 +0100

    added other types of trees

--- Tree/src/main/main.java
+++ Tree/src/main/main.java
@@ -1,12 +1,11 @@
 package main;
 
 import tree.BinaryTree;
-import tree.Tree;
 
 public class main {
     public static void main(String[] args) {
-        Tree t = new BinaryTree();
+        BinaryTree t = new BinaryTree();
         t.insert(10);
-        System.out.println(t.size());
+        System.out.println(t.search(10));
     }
 }
