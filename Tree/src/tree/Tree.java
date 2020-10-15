f249a39d3adaca19307a0a9387d3976e748decdc
Prashant-JT <prashantjeswani8@gmail.com>
2020-10-15 23:04:58 +0100

    Final version

--- Tree/src/tree/Tree.java
+++
@@ -1,9 +0,0 @@
-package tree;
-
-public interface Tree {
-    public boolean insert(int e);
-    public boolean remove(int e);
-    public boolean search(int e);
-    //public int size();
-    //public void empty();
-}
